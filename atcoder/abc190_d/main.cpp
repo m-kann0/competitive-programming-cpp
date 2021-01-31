fc24a306a32a0abdda08d627252fe2c7752e29de
kanno <m.kanno.0915@gmail.com>
2021-02-01 08:29:22 +0900

    implement by editorial way

--- atcoder/abc190_d/main.cpp
+++ atcoder/abc190_d/main.cpp
@@ -6,17 +6,45 @@ using namespace atcoder;
 #define rep(i, n) for (int i = 0; i < (n); i++)
 using ll = long long;
 
-int main() {
-    ll s;
-    cin >> s;
+//int main() {
+//    ll s;
+//    cin >> s;
+//
+//    ll ans = 0;
+//    for (ll n = 1; n * n < s * 2; n++) {
+//        if ((2 * s - n * n + n) % (2 * n) == 0) {
+//            ans += 2;
+//        }
+//    }
+//    cout << ans << endl;
+//
+//    return 0;
+//}
 
-    ll ans = 0;
-    for (ll n = 1; n * n < s * 2; n++) {
-        if ((2 * s - n * n + n) % (2 * n) == 0) {
-            ans += 2;
+vector<ll> enumerate_divisors(ll n) {
+    vector<ll> res;
+    for (ll i = 1; i * i <= n; i++) {
+        if (n % i == 0) {
+            res.push_back(i);
+            if (n / i != i) {
+                res.push_back(n / i);
+            }
+        }
+    }
+    sort(res.begin(), res.end());
+    return res;
+}
+
+int main() {
+    ll n;
+    cin >> n;
+    vector<ll> divisors = enumerate_divisors(2 * n);
+    int ans = 0;
+    for (auto l : divisors) {
+        if ((2 * n / l - l + 1) % 2 == 0) {
+            ans++;
         }
     }
     cout << ans << endl;
-
     return 0;
 }
