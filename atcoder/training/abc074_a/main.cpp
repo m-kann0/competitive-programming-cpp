fdd0ad297b39bdace3d061ac40784ead274c753c
kanno <m.kanno.0915@gmail.com>
2021-02-07 04:00:46 +0900

    AC

---
+++ atcoder/training/abc074_a/main.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+
+#define rep(i, n) for (int i = 0; i < (n); i++)
+using ll = long long;
+
+int main() {
+    int N, A;
+    cin >> N >> A;
+
+    cout << (N * N - A) << endl;
+
+    return 0;
+}
