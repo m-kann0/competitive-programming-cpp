a4e9b195a111ad8d0aa75a210f8c465fdd3053c6
m-kann0 <m.kanno.0915@gmail.com>
2021-02-13 08:53:36 +0900

    AC

---
+++ atcoder/training/abc053_a/main.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+
+#define rep(i, n) for (int i = 0; i < (n); i++)
+using ll = long long;
+
+int main() {
+    int x;
+    cin >> x;
+
+    if (x < 1200) {
+        cout << "ABC" << endl;
+    } else {
+        cout << "ARC" << endl;
+    }
+
+    return 0;
+}
