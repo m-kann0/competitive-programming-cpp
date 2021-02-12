d1609239535696ab2f80cd512a74c8a8784c3ad2
kanno <m.kanno.0915@gmail.com>
2021-02-12 22:28:24 +0900

    AC

---
+++ atcoder/training/abc006_1/main.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+
+#define rep(i, n) for (int i = 0; i < (n); i++)
+using ll = long long;
+
+int main() {
+    int N;
+    cin >> N;
+
+    if (N % 3 == 0) {
+        cout << "YES" << endl;
+    } else {
+        cout << "NO" << endl;
+    }
+
+    return 0;
+}
