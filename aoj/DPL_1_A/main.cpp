b4b2fee2368e9d9b5197c99e8476a92995b9b816
kanno <m.kanno.0915@gmail.com>
2021-02-14 08:37:54 +0900

    AC

---
+++ aoj/DPL_1_A/main.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, n) for (int i = 0; i < (n); i++)
+using ll = long long;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    int c[m];
+    rep(i, m) cin >> c[i];
+
+    const int INF = 1'000'000'000;
+    vector<vector<int>> dp(m + 1, vector<int>(n + 1, INF));
+    rep(i, m + 1) dp[i][0] = 0;
+    rep(i, m) {
+        rep(j, n + 1) {
+            dp[i + 1][j] = min(dp[i + 1][j], dp[i][j]);
+            if (j >= c[i]) {
+                dp[i + 1][j] = min(dp[i + 1][j], dp[i + 1][j - c[i]] + 1);
+            }
+        }
+    }
+    cout << dp[m][n] << endl;
+
+    return 0;
+}
