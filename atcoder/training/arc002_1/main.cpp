7e8f5b46f4c02c2f840807e5154ee5280090e09b
m-kann0 <m.kanno.0915@gmail.com>
2021-02-13 11:14:28 +0900

    AC

---
+++ atcoder/training/arc002_1/main.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+
+#define rep(i, n) for (int i = 0; i < (n); i++)
+using ll = long long;
+
+int main() {
+    int Y;
+    cin >> Y;
+
+    auto f = [](int year) {
+        if (year % 400 == 0) {
+            return true;
+        }
+        if (year % 100 == 0) {
+            return false;
+        }
+        if (year % 4 == 0) {
+            return true;
+        }
+        return false;
+    };
+
+    if (f(Y)) {
+        cout << "YES" << endl;
+    } else {
+        cout << "NO" << endl;
+    }
+
+    return 0;
+}
