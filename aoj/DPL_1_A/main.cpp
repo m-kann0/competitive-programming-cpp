34cc9dabd8fe4f709b66be7670dd8956da6b3f6c
kanno <m.kanno.0915@gmail.com>
2021-02-14 08:42:54 +0900

    using chmin

--- aoj/DPL_1_A/main.cpp
+++ aoj/DPL_1_A/main.cpp
@@ -4,6 +4,14 @@ using namespace std;
 #define rep(i, n) for (int i = 0; i < (n); i++)
 using ll = long long;
 
+template<class T> inline bool chmin(T& a, T b) {
+    if (a > b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+
 int main() {
     int n, m;
     cin >> n >> m;
@@ -15,9 +23,11 @@ int main() {
     rep(i, m + 1) dp[i][0] = 0;
     rep(i, m) {
         rep(j, n + 1) {
-            dp[i + 1][j] = min(dp[i + 1][j], dp[i][j]);
+            // dp[i + 1][j] = min(dp[i + 1][j], dp[i][j]);
+            chmin(dp[i + 1][j], dp[i][j]);
             if (j >= c[i]) {
-                dp[i + 1][j] = min(dp[i + 1][j], dp[i + 1][j - c[i]] + 1);
+                // dp[i + 1][j] = min(dp[i + 1][j], dp[i + 1][j - c[i]] + 1);
+                chmin(dp[i + 1][j], dp[i + 1][j - c[i]] + 1);
             }
         }
     }
