46dd92ab930d25ac3a1005c0f1fceb67a5cbcf75
kanno <m.kanno.0915@gmail.com>
2021-02-21 09:40:44 +0900

    editorial AC

--- atcoder/contest/abc192/abc192_d/main.cpp
+++ atcoder/contest/abc192/abc192_d/main.cpp
@@ -6,92 +6,54 @@ using namespace atcoder;
 using ll = long long;
 using P = pair<ll, ll>;
 
-#define rep(i, n) for (ll i = 0; i < (n); i++)
-
-const ll INF = 1'000'000'000'000'000'005;
-
-ll calc(string X, ll d, ll M) {
-    ll res = 0;
-    rep(i, X.size()) {
-        res *= d;
-        res += X[i] - '0';
-        if (res > M) {
-            return INF;
-        }
-    }
-    return res;
-}
-
-vector<ll> calc2(ll M, ll d) {
-    vector<ll> res;
-    while (M > 0) {
-        res.push_back(M % d);
-        M /= d;
-    }
-    reverse(res.begin(), res.end());
-    return res;
-}
-
-bool larger_than(vector<ll> X, vector<ll> Y) {
-    if (X.size() > Y.size()) {
-        return true;
-    }
-    if (X.size() < Y.size()) {
-        return false;
-    }
-    rep(i, X.size()) {
-        if (X[i] == Y[i]) {
-            continue;
-        }
-        if (X[i] > Y[i]) {
-            return true;
-        }
-        if (X[i] < Y[i]) {
-            return false;
-        }
-    }
-    return false;
-}
+#define rep(i, n) for (ll i = 0; i < ((ll) n); i++)
 
 int main() {
     string X;
     ll M;
     cin >> X >> M;
 
+    if (X.size() == 1) {
+        if (X[0] - '0' <= M) {
+            cout << 1 << endl;
+        } else {
+            cout << 0 << endl;
+        }
+        return 0;
+    }
+
     ll d = 0;
     rep(i, X.size()) {
         d = max((int) d, X[i] - '0');
     }
 
-    vector<ll> X2;
-    rep(i, X.size()) {
-        X2.push_back(X[i] - '0');
-    }
+    auto is_ok = [&](ll n) {
+        ll value = 0;
+        rep(i, X.size()) {
+            if (value > (M + n - 1) / n) {
+                return false;
+            }
+            value *= n;
+            value += X[i] - '0';
+        }
+        if (value > M) {
+            return false;
+        }
+        return true;
+    };
 
-    const ll INF = 1'000'000'000'000'000'000;
     ll ok = d;
-    ll ng = INF;
+    ll ng = M + 1;
     while (abs(ok - ng) > 1) {
         ll mid = (ok + ng) / 2;
-        if (!larger_than(X2, calc2(M, mid))) {
+        if (is_ok(mid)) {
             ok = mid;
         } else {
             ng = mid;
         }
     }
 
-    ll ans = ok - d;
-
-    // ll ans = 0;
-    // while (true) {
-    //     vector<ll> now = calc2(M, d);
-    //     if (larger_than(X2, now)) {
-    //         break;
-    //     }
-    //     ans++;
-    //     d++;
-    // }
-    cout << ans << endl;
+    cout << ok - d << endl;
 
     return 0;
 }
